95b75bf22a7bb134e8369e012c462aec6418dad7
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2021-01-06 19:47:48 -0800

    first.js is the server file

--- first.js
+++ first.js
@@ -1,6 +1,9 @@
 var http = require('http');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+    fs.readFile('./main.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data, 'utf-8');
+    });
+
 }).listen(8080);
